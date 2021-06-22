f41970fc9c6480e9dbc097eec7ca68d4cf3646d2
Morgan Shadung <morgan@shadung.com>
2021-06-22 20:27:39 +0200

    Create handler functions to handle each endpoint. Register all the routes too.

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -14,8 +16,17 @@ type Book struct {
 	Year   string `json:"year"`
 }
 
+var books []Book
+
 func main() {
 	router := mux.NewRouter()
+
+	books = append(books,
+		Book{ID: 1, Title: "Learn your ABCs", Author: "Bob YourUncle", Year: "2021"},
+		Book{ID: 2, Title: "Count your 123s", Author: "Yellow Sun", Year: "2022"},
+		Book{ID: 3, Title: "Read your Code", Author: "Brown Dog", Year: "2023"},
+		Book{ID: 4, Title: "Study your ABCs", Author: "Grey Cat", Year: "2024"},
+	)
 
 	router.HandleFunc("/books", getBooks).Methods("GET")
 	router.HandleFunc("/books/{id}", getBook).Methods("GET")
@@ -27,21 +38,57 @@ func main() {
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	log.Println("Get all the books")
+	json.NewEncoder(w).Encode(books)
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("Get a single book")
+	params := mux.Vars(r)
+
+	i, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	for _, book := range books {
+		if book.ID == i {
+			json.NewEncoder(w).Encode(&book)
+		}
+	}
 }
 
 func addBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("Add a single book")
+	var book Book
+
+	_ = json.NewDecoder(r.Body).Decode(&book)
+
+	books = append(books, book)
+
+	json.NewEncoder(w).Encode(books)
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("Update a single book")
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
+
+	for i, item := range books {
+		if item.ID == book.ID {
+			books[i] = book
+		}
+	}
+
+	json.NewEncoder(w).Encode(books)
 }
 
 func removeBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("Delete a single book")
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	for i, item := range books {
+		if item.ID == id {
+			books = append(books[:i], books[i+1:]...)
+		}
+	}
+
+	json.NewEncoder(w).Encode(books)
 }
