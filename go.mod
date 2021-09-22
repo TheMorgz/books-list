1c7dd6c2423a15074da87d6065fc40fbb55c6d12
TheMorgz <morgan@shadung.com>
2021-09-22 18:25:50 +0200

    Add CORS config

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module books-list
 go 1.16
 
 require (
+	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // direct
 	github.com/lib/pq v1.10.2 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"books-list/controllers"
 	"books-list/driver"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -34,5 +35,6 @@ func main() {
 	router.HandleFunc("/books/{id}", controller.RemoveBook(db)).Methods("DELETE")
 
 	fmt.Println("Server is running at port 8000")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"}),
+		handlers.AllowedOrigins([]string{"*"}))(router)))
 }
